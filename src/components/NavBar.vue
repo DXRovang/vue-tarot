8b3cbbe65dbc2b6234d9ff257d88881e6ca27f55
Alexander Rovang <DXRovang@gmail.com>
2023-04-30 12:25:21 -0400

    experiments with bootstrap

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="nav">
-    <h1>Readings</h1>
-    <h1>New Card</h1>
-    <h1>Random Card</h1>
-    <h1>Major Arcana</h1>
-    <h1>Minor Arcana</h1>
+<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Readings</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">New Draw</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Random Card</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Major Arcana</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Minor Arcana</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
+</nav>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+// import Vue from 'vue'
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
+// import { BootstrapVue } from 'bootstrap-vue'
+
 
 createApp(App).mount('#app')
+// Vue.use(BootstrapVue)
+import "bootstrap/dist/js/bootstrap.js"
