517accdcdd65d0d593326458273de011a70d4750
Alexander Rovang <DXRovang@gmail.com>
2023-05-03 11:41:37 -0400

    more css

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="card carx">
-  <img :src="card.image" class="card-img-top carx" alt="Card Image" style="width: 18rem;" />
-  <div class="card-body">
+<div class="card" style="width: 13vw">
+  <img :src="card.image" class="card-img-top" alt="Card Image" style="width: 18rem; margin: auto;" />
+  <div class="card-body col-auto mb-3">
     <h4 class="card-text" style="background-color: white">{{ card.name }}</h4>
     <a href="#" class="btn btn-primary"></a>
   </div>
@@ -21,5 +21,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+ .card{
+  border-style: solid;
+  border-width: 2px;
+  border-color: black;
+  margin: 10px;
+  padding: 2px;
+ }
+ .card-img-top {
+    width: 100%;
+    height: 20vh;
+    object-fit: cover;
+}
 </style>
